934ab6af0c1eeb93a82f8e58f6ba0496cf14a16e
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2026-05-22 12:24:49 -0400

    Install posthog session replay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Suspense } from "react";
 import { SanityLive } from "@/sanity/lib/live";
 import ChatWidgetLoader from "@/features/chat/components/chat-widget-loader";
 import { PageViewTracker } from "@/components/page-view-tracker";
+import { PostHogProvider } from "@/components/posthog-provider";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-geist-sans",
@@ -155,12 +156,14 @@ export default function RootLayout({
       className={`${plusJakartaSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Suspense>
-          <PageViewTracker />
-        </Suspense>
-        {children}
-        <SanityLive />
-        <ChatWidgetLoader />
+        <PostHogProvider>
+          <Suspense>
+            <PageViewTracker />
+          </Suspense>
+          {children}
+          <SanityLive />
+          <ChatWidgetLoader />
+        </PostHogProvider>
       </body>
     </html>
   );
