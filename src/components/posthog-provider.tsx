179f0d31631cddc3870ca8f5b0e4a1c5e9ba7e54
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2026-05-22 12:37:47 -0400

    Fix bug

--- src/components/posthog-provider.tsx
+++ src/components/posthog-provider.tsx
@@ -6,11 +6,15 @@ import { useEffect } from "react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
+    if (!process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN || posthog.__loaded)
+      return;
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
       session_recording: {
         maskAllInputs: false,
       },
+      disable_external_dependency_loading: true,
     });
   }, []);
 
